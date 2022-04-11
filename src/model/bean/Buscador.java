507d2efa6cf7e9ed9b2efd5070eb4f75673d1265
Pedro <pedro_citadin@outlook.com>
2022-04-11 19:20:31 -0300

    Versão base

---
+++ src/model/bean/Buscador.java
@@ -0,0 +1,56 @@
+
+package model.bean;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import model.util.Arquivo;
+
+/**
+ *
+ * @author Pedro Citadin Coelho
+ */
+public class Buscador {
+        public List<File> listaArquivos;
+
+    public List<File> getListaArquivos() {
+        return listaArquivos;
+    }
+
+    public void setListaArquivos(List<File> listaArquivos) {
+        this.listaArquivos = listaArquivos;
+    }
+
+    public Buscador() {
+    }
+
+    public Buscador(List<File> listaArquivos) {
+        this.listaArquivos = listaArquivos;
+    }
+    
+    public void pesquisaNome(String nome, String diretorio) throws IOException{
+        listaArquivos = Arquivo.pegaTodosOsArquivos(diretorio);
+        for (File f : listaArquivos) {
+            Arquivo a = new Arquivo(f);
+            List<String> listaLinhas = a.pegalistaLinhas(a);
+            int contador = 0;
+            for (String linha : listaLinhas) {
+                String[] palavras = linha.split(" ");
+                contador++;
+                for (int i = 0; i < palavras.length; i++) {
+                    if (palavras[i].equalsIgnoreCase(nome)) {
+                    System.out.println("Encontrado no arquivo: "+a.getNome()+" na linha: "+contador);
+                    }
+                }
+            }
+        }
+        
+        
+    }
+        
+        
+        
+        
+        
+        
+}
---
+++ src/model/exe/Main.java
@@ -0,0 +1,26 @@
+
+package model.exe;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import model.bean.Buscador;
+import model.util.Arquivo;
+
+/**
+ *
+ * @author Pedro Citadin Coelho
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Buscador busc = new Buscador();
+        
+        long inicio = System.currentTimeMillis();
+        busc.pesquisaNome("CRISTO", "lista");
+        long fim = System.currentTimeMillis();
+        long tempo = fim - inicio;
+        System.out.println("Tempo decorrido: "+tempo+" milissegundos");
+        
+        
+    }
+}
