df44348d93883f5a198754dced7a1676d64c664f
Pedro Citadin Coelho <36762723+PedroCitadin@users.noreply.github.com>
2022-04-18 13:23:13 -0300

    Update Buscador.java

--- src/model/bean/Buscador.java
+++ src/model/bean/Buscador.java
@@ -30,6 +30,9 @@ public class Buscador {
 
     public void pesquisaNome(String nome, File diretorio) throws IOException {
         listaArquivos = Arquivo.pegaTodosOsArquivos(diretorio);
+        
+        ////busca sequencial na lista dos arquivos do diretório, cada é arquivo é lido, um de cada vez e cada linha do arquivo também, caso o nome seja encontrado, o sistema
+        ///informa o nome do arquivo e a linha
         for (File f : listaArquivos) {
             Arquivo a = new Arquivo(f);
             List<String> listaLinhas = a.pegalistaLinhas(a);
